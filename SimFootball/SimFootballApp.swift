d5df9c88a9fccd232a9abde8c4b395e7e6c2184c
Sabri Benhadda <sabri.benhadda@gmail.com>
2025-11-23 14:09:13 +0100

    Initial Commit

---
+++ SimFootball/SimFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimFootballApp.swift
+//  SimFootball
+//
+//  Created by Sabri Benhadda on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
